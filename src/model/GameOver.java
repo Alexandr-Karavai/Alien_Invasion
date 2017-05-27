0150fcc1764017705b3048dac7e620656df2eb05
karavai <karavai96-2010@mail.ru>
2017-05-27 21:29:17 +0300

    Added a message about winning and losing

--- src/model/GameOver.java
+++ src/model/GameOver.java
@@ -1,7 +1,27 @@
 package model;
 
-/**
- * Created by karavai on 25.11.16.
- */
+import javax.swing.*;
+
 public class GameOver {
+
+    private String stringLose = "/res/gameOver2.png";
+
+    private final int START_Y = 200;
+    private final int START_X = 160;
+    public Sprite spriteLose;
+    public int width;
+
+    public GameOver(Sprite spriteLose) {
+        this.spriteLose = spriteLose;
+
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(stringLose));
+
+        width = ii.getImage().getWidth(null);
+
+        spriteLose.setX(START_X);
+        spriteLose.setY(START_Y);
+        spriteLose.setImage(ii.getImage());
+
+    }
 }
+
--- src/model/GameWin.java
+++ src/model/GameWin.java
@@ -1,7 +1,28 @@
 package model;
 
+import javax.swing.*;
+
 /**
  * Created by karavai on 24.11.16.
  */
 public class GameWin {
+    private String player = "/res/win.png";
+
+    private final int START_Y = 200;
+    private final int START_X = 220;
+    public Sprite spriteWin;
+    public int width;
+
+    public GameWin(Sprite spriteWin) {
+        this.spriteWin = spriteWin;
+
+        ImageIcon ii = new ImageIcon(this.getClass().getResource(player));
+
+        width = ii.getImage().getWidth(null);
+
+        spriteWin.setX(START_X);
+        spriteWin.setY(START_Y);
+        spriteWin.setImage(ii.getImage());
+
+    }
 }
