d3cb1277d85c6fa545e7dbcfb0406a959803ab40
karavai <karavai96-2010@mail.ru>
2017-05-27 21:26:53 +0300

    Added main menu

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,17 +1,47 @@
-import javazoom.jl.player.Player;
+import view.NewGame;
 import view.ScreenView;
 
 import javax.swing.*;
 
-import java.io.FileInputStream;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static controller.Settings.*;
 
 public class MainGame extends JFrame  {
 
-    public MainGame() {
+    private JButton newGame;
+    private JButton exitGame;
+    private JPanel panelMenu;
 
-        add(new ScreenView());
+
+    public MainGame() {
+        ImageIcon icon = new ImageIcon(this.getClass().getResource("res/12.jpg"));
+        ImageIcon iconExit = new ImageIcon(this.getClass().getResource("res/exit.jpg"));
+        panelMenu = new JPanel();
+        panelMenu.setLayout(new GridLayout(2,1,50,0));
+        newGame = new JButton();
+        newGame.setIcon(icon);
+      //  newGame.setSize(48,48);
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                new NewGame();
+            }
+        });
+        panelMenu.add(newGame);
+        exitGame = new JButton();
+        exitGame.setIcon(iconExit);
+        exitGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        panelMenu.add(exitGame);
+        add(panelMenu);
         setTitle("Kill Alien");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -22,14 +52,7 @@ public class MainGame extends JFrame  {
     }
 
     public static void main(String[] args) {
-
-        try{
-            FileInputStream fis = new FileInputStream("src/res/main.mp3");
-            Player playMP3 = new Player(fis);
             new MainGame();
-            playMP3.play();
-        }catch(Exception e){System.out.println(e);}
-
     }
 
 }
