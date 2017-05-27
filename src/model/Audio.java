a6032020581ea90111d5efe63af85a1433abc7b2
karavai <karavai96-2010@mail.ru>
2017-05-27 21:30:08 +0300

    Added audio

--- src/model/Audio.java
+++ src/model/Audio.java
@@ -1,7 +1,40 @@
 package model;
 
-/**
- * Created by karavai on 25.11.16.
- */
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
 public class Audio {
+
+    private final String urlString;
+    private URL url;
+    private final AudioClip audioClip;
+
+    public Audio(String urlString) {
+
+        this.urlString = urlString;
+        url = this.getClass().getResource(urlString);
+        audioClip = Applet.newAudioClip(url);
+    }
+
+    public void play() {
+        audioClip.play();
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getUrlString() {
+        return urlString;
+    }
+
+    public AudioClip getAudioClip() {
+        return audioClip;
+    }
 }
